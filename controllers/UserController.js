c982288614c3882647aae0e47a49f2594c93a695
Artur Gajewski <artur-gajewski@fraktio.fi>
2015-10-16 10:20:37 +0300

    Initial commit

---
+++ controllers/UserController.js
@@ -0,0 +1,30 @@
+(function() {
+
+  var app = angular.module("githubViewer");
+
+  var UserController = function($scope, github, $routeParams) {
+
+    var onUser = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError);
+    };
+    
+    var onRepos = function(data) {
+      $scope.repos = data;
+    };
+    
+    var onError = function(reason) {
+      $scope.error = "Could not fetch user!";
+    }
+
+    $scope.username = $routeParams.username;
+    $scope.repoSortOrder = '-stargazers_count';
+    github.getUser($scope.username).then(onUser, onError);
+
+  };  
+
+  app.controller("UserController", UserController);
+
+}());
+
+
